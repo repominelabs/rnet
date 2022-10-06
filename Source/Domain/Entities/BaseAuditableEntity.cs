09405552b8aaef8b9b16105e54b5a26eaeac1918
Ömer Çelik <oclk@outlook.com.tr>
2022-10-06 13:09:22 +0300

    feat: Added BaseAuditableEntity class

---
+++ Source/Domain/Entities/BaseAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public abstract class BaseAuditableEntity
+    {
+        public DateTime CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
