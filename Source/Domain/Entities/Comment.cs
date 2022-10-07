3b8e49cb6f795701bc55fede71894b37368ea350
Ömer Çelik <oclk@outlook.com.tr>
2022-10-08 00:16:19 +0300

    reafactor: Added column tag to all entities

--- Source/Domain/Entities/Comment.cs
+++ Source/Domain/Entities/Comment.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
     public class Comment : BaseAuditableEntity
     {
+        [Column("post_id")]
         public int PostId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
--- Source/Domain/Entities/Post.cs
+++ Source/Domain/Entities/Post.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
     public class Post : BaseAuditableEntity
     {
+        [Column("user_id")]
         public int UserId { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
