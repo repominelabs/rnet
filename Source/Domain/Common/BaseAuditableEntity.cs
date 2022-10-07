8322de029a2049613c634c648c5f1aeebd5d6d2f
Ömer Çelik <oclk@outlook.com.tr>
2022-10-07 17:47:36 +0300

    feat: Added Common models (BaseAuditableEntity, BaseEntity, BaseEvent)

--- Source/Domain/Common/BaseAuditableEntity.cs
+++ Source/Domain/Common/BaseAuditableEntity.cs
@@ -1,6 +1,6 @@
-﻿namespace Domain.Entities
+﻿namespace Domain.Common
 {
-    public abstract class BaseAuditableEntity
+    public abstract class BaseAuditableEntity : BaseEntity
     {
         public DateTime CreatedDate { get; set; }
         public string? CreatedBy { get; set; }
