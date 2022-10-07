e849891aa017d780d687ca63e166c53a96cf9c53
Ömer Çelik <oclk@outlook.com.tr>
2022-10-07 19:26:54 +0300

    feat: Added Post and Comment entities

---
+++ Source/Domain/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Comment : BaseAuditableEntity
+    {
+        public int PostId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Source/Domain/Entities/Post.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Post : BaseAuditableEntity
+    {
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
