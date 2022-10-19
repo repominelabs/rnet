efb87c9d9024519a646e660d07a25f6f8913881e
Ömer Çelik <oclk@outlook.com.tr>
2022-10-19 15:20:08 +0300

    refactor: Rollback all changes in IBaseRepository file

--- Source/Application/Interfaces/Repositories/IBaseRepository.cs
+++ Source/Application/Interfaces/Repositories/IBaseRepository.cs
@@ -1,21 +1,21 @@
 ﻿namespace Application.Interfaces.Repositories;
 
-public interface IBaseRepository
+public interface IBaseRepository<T>
 {
-    dynamic Create<T>(T entity);
-    Task<dynamic> CreateAsync<T>(T entity);
-    dynamic CreateOrUpdate<T>(T entity, bool nullable = false, string whereClause = null);
-    Task<dynamic> CreateOrUpdateAsync<T>(T entity, bool nullable = false, string whereClause = null);
-    dynamic Delete<T>(dynamic id);
-    dynamic Delete<T>(string whereClause);
-    Task<dynamic> DeleteAsync<T>(dynamic id);
-    Task<dynamic> DeleteAsync<T>(string whereClause);
-    List<T> Get<T>(dynamic id);
-    List<T> Get<T>(string whereClause = null);
-    Task<List<T>> GetAsync<T>(dynamic id);
-    Task<List<T>> GetAsync<T>(string whereClause = null);
-    dynamic Update<T>(T entity, bool nullable = false, string whereClause = null);
-    Task<dynamic> UpdateAsync<T>(T entity, bool nullable = false, string whereClause = null);
-    List<T> Query<T>();
-    Task<List<T>> QueryAsync<T>();
+    dynamic Create(T entity);
+    Task<dynamic> CreateAsync(T entity);
+    dynamic CreateOrUpdate(T entity, bool nullable = false, string whereClause = null);
+    Task<dynamic> CreateOrUpdateAsync(T entity, bool nullable = false, string whereClause = null);
+    dynamic Delete(dynamic id);
+    dynamic Delete(string whereClause);
+    Task<dynamic> DeleteAsync(dynamic id);
+    Task<dynamic> DeleteAsync(string whereClause);
+    List<T> Get(dynamic id);
+    List<T> Get(string whereClause = null);
+    Task<List<T>> GetAsync(dynamic id);
+    Task<List<T>> GetAsync(string whereClause = null);
+    dynamic Update(T entity, bool nullable = false, string whereClause = null);
+    Task<dynamic> UpdateAsync(T entity, bool nullable = false, string whereClause = null);
+    List<T> Query();
+    Task<List<T>> QueryAsync();
 }
