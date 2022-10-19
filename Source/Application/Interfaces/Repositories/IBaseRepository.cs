f4a348341d230b832e2c4843efbb592113e3eb53
Ömer Çelik <oclk@outlook.com.tr>
2022-10-19 15:02:17 +0300

    refactor: Added querry prams

--- Source/Application/Interfaces/Repositories/IBaseRepository.cs
+++ Source/Application/Interfaces/Repositories/IBaseRepository.cs
@@ -4,8 +4,8 @@ public interface IBaseRepository
 {
     dynamic Create<T>(T entity);
     Task<dynamic> CreateAsync<T>(T entity);
-    dynamic CreateOrUpdate<T>(T entity);
-    Task<dynamic> CreateOrUpdateAsync<T>(T entity);
+    dynamic CreateOrUpdate<T>(T entity, bool nullable = false, string whereClause = null);
+    Task<dynamic> CreateOrUpdateAsync<T>(T entity, bool nullable = false, string whereClause = null);
     dynamic Delete<T>(dynamic id);
     dynamic Delete<T>(string whereClause);
     Task<dynamic> DeleteAsync<T>(dynamic id);
