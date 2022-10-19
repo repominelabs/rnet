4480327bf34d7559f0fcf5e9c756dc768263dd73
Ömer Çelik <oclk@outlook.com.tr>
2022-10-19 15:18:23 +0300

    feat: Created IDatabaseManagerRepository file

---
+++ Source/Application/Interfaces/Repositories/IDatabaseManagerRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.Interfaces.Repositories;
+
+public interface IDatabaseManagerRepository
+{
+    dynamic Create<T>(T entity);
+    Task<dynamic> CreateAsync<T>(T entity);
+    dynamic CreateOrUpdate<T>(T entity, bool nullable = false, string whereClause = null);
+    Task<dynamic> CreateOrUpdateAsync<T>(T entity, bool nullable = false, string whereClause = null);
+    dynamic Delete<T>(dynamic id);
+    dynamic Delete<T>(string whereClause);
+    Task<dynamic> DeleteAsync<T>(dynamic id);
+    Task<dynamic> DeleteAsync<T>(string whereClause);
+    List<T> Get<T>(dynamic id);
+    List<T> Get<T>(string whereClause = null);
+    Task<List<T>> GetAsync<T>(dynamic id);
+    Task<List<T>> GetAsync<T>(string whereClause = null);
+    dynamic Update<T>(T entity, bool nullable = false, string whereClause = null);
+    Task<dynamic> UpdateAsync<T>(T entity, bool nullable = false, string whereClause = null);
+    List<T> Query<T>();
+    Task<List<T>> QueryAsync<T>();
+}
