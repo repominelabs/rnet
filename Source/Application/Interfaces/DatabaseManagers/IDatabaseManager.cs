2e56ada9e5d2be4eb5819d897d081288a962ee51
Ömer Çelik <oclk@outlook.com.tr>
2022-10-20 09:59:55 +0300

    refactor: Clear useless methods

--- Source/Application/Interfaces/DatabaseManagers/IDatabaseManager.cs
+++ Source/Application/Interfaces/DatabaseManagers/IDatabaseManager.cs
@@ -4,28 +4,16 @@ namespace Application.Interfaces.DatabaseManagers;
 
 public interface IDatabaseManager
 {
-    dynamic Create<T>(T entity);
-    dynamic Create<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, T entity);
-    Task<dynamic> CreateAsync<T>(T entity);
-    Task<dynamic> CreateAsync<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, T entity);
-    dynamic Delete<T>(string whereClause);
-    dynamic Delete<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, string whereClause);
-    Task<dynamic> DeleteAsync<T>(string whereClause);
-    Task<dynamic> DeleteAsync<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, string whereClause);
-    List<T> Get<T>(string whereClause = null);
-    List<T> Get<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, string whereClause = null);
-    Task<List<T>> GetAsync<T>(string whereClause = null);
-    Task<List<T>> GetAsync<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, string whereClause = null);
-    List<T> Query<T>(string query);
-    List<T> Query<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, string query);
-    List<T> Query<T>(string query, object parameters);
-    List<T> Query<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, string query, object parameters);
-    Task<List<T>> QueryAsync<T>(string query);
-    Task<List<T>> QueryAsync<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, string query);
-    Task<List<T>> QueryAsync<T>(string query, object parameters);
-    Task<List<T>> QueryAsync<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, string query, object parameters);
-    dynamic Update<T>(T entity, bool nullable = false, string whereClause = null);
-    dynamic Update<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, T entity, bool nullable = false, string whereClause = null);
-    Task<dynamic> UpdateAsync<T>(T entity, bool nullable = false, string whereClause = null);
-    Task<dynamic> UpdateAsync<T>(IDbConnection dbConnection, IDbTransaction dbTransaction, T entity, bool nullable = false, string whereClause = null);
+    dynamic Create<T>(T entity, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    Task<dynamic> CreateAsync<T>(T entity, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    dynamic Delete<T>(string whereClause = null, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    Task<dynamic> DeleteAsync<T>(string whereClause = null, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    List<T> Get<T>(string whereClause = null, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    Task<List<T>> GetAsync<T>(string whereClause = null, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    List<T> Query<T>(string query, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    List<T> Query<T>(string query, object parameters, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    Task<List<T>> QueryAsync<T>(string query, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    Task<List<T>> QueryAsync<T>(string query, object parameters, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    dynamic Update<T>(T entity, bool nullable = false, string whereClause = null, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
+    Task<dynamic> UpdateAsync<T>(T entity, bool nullable = false, string whereClause = null, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null);
 }
