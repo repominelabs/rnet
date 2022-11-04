62d793fa0b74af9c40d8ad05607a73d30d419ab1
Ömer Çelik <oclk@outlook.com.tr>
2022-11-04 16:11:19 +0300

    refactor: Updated base repo interface with new funcs

--- Source/Application/Interfaces/Repositories/IBaseRepository.cs
+++ Source/Application/Interfaces/Repositories/IBaseRepository.cs
@@ -4,6 +4,17 @@ namespace Application.Interfaces.Repositories;
 
 public interface IBaseRepository<T>
 {
+    // Sync Func(s) 
+    dynamic Create(T entity);
     dynamic CreateOrUpdate(T entity, bool nullable = false, string whereClause = null, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null, DbType dbType = DbType.Int32);
+    List<T> Read(dynamic id = null, string whereClause = null);
+    dynamic Update(T entity, bool nullable = false, string? whereClause = null);
+    dynamic Delete(dynamic id = null, string whereClause = null);
+
+    // Async Func(s)
+    Task<dynamic> CreateAsync(T entity);
     Task<dynamic> CreateOrUpdateAsync(T entity, bool nullable = false, string whereClause = null, IDbConnection dbConnection = null, IDbTransaction dbTransaction = null, DbType dbType = DbType.Int32);
+    Task<List<T>> ReadAsync(dynamic id = null, string whereClause = null);
+    Task<dynamic> UpdateAsync(T entity, bool nullable = false, string? whereClause = null);
+    Task<dynamic> DeleteAsync(dynamic id = null, string whereClause = null);
 }
