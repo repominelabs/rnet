baaef81ef07e604763047624c73501fdc916a3f1
Ömer Çelik <oclk@outlook.com.tr>
2022-11-08 18:07:56 +0300

    refactor: Changed all services types

--- Source/Infrastructure/Persistence/Repositories/CommentRepository.cs
+++ Source/Infrastructure/Persistence/Repositories/CommentRepository.cs
@@ -1,12 +1,11 @@
-﻿using Application.Interfaces.DatabaseManagers;
-using Application.Interfaces.Repositories;
+﻿using Application.Interfaces.Repositories;
 using Domain.Entities;
 
 namespace Infrastructure.Persistence.Repositories;
 
 public class CommentRepository : BaseRepository<Comment>, ICommentRepository
 {
-    public CommentRepository(string connStr, IDatabaseManager databaseManager) : base(connStr, databaseManager)
+    public CommentRepository(string connStr) : base(connStr)
     {
     }
 }
--- Source/Infrastructure/Persistence/Repositories/PostRepository.cs
+++ Source/Infrastructure/Persistence/Repositories/PostRepository.cs
@@ -1,12 +1,11 @@
-﻿using Application.Interfaces.DatabaseManagers;
-using Application.Interfaces.Repositories;
+﻿using Application.Interfaces.Repositories;
 using Domain.Entities;
 
 namespace Infrastructure.Persistence.Repositories;
 
 public class PostRepository : BaseRepository<Post>, IPostRepository
 {
-    public PostRepository(string connStr, IDatabaseManager databaseManager) : base(connStr, databaseManager)
+    public PostRepository(string connStr) : base(connStr)
     {
     }
 }
