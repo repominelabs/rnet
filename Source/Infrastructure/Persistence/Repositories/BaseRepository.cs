cfa39326a12b2ab713082c9733964e175b7fdab0
Ömer Çelik <oclk@outlook.com.tr>
2022-11-08 18:03:24 +0300

    refactor: Removed create or update old methods

--- Source/Infrastructure/Persistence/Repositories/BaseRepository.cs
+++ Source/Infrastructure/Persistence/Repositories/BaseRepository.cs
@@ -99,34 +99,4 @@ public class BaseRepository<T> : IBaseRepository<T>
     {
         throw new NotImplementedException();
     }
-
-    //public dynamic CreateOrUpdate(T entity, bool nullable = false, string? whereClause = null, IDbConnection? dbConnection = null, IDbTransaction? dbTransaction = null, DbType dbType = DbType.Int32)
-    //{
-    //    T? response = _databaseManager.Get<T>(whereClause, dbConnection, dbTransaction).FirstOrDefault();
-    //    if (response == null)
-    //    {
-    //        dynamic result = _databaseManager.Create(entity, dbConnection, dbTransaction, dbType);
-    //        return result;
-    //    }
-    //    else
-    //    {
-    //        dynamic result = _databaseManager.Update(entity, nullable, whereClause, dbConnection, dbTransaction);
-    //        return result;
-    //    }
-    //}
-
-    //public async Task<dynamic> CreateOrUpdateAsync(T entity, bool nullable = false, string? whereClause = null, IDbConnection? dbConnection = null, IDbTransaction? dbTransaction = null, DbType dbType = DbType.Int32)
-    //{
-    //    var response = await _databaseManager.GetAsync<T>(whereClause, dbConnection, dbTransaction);
-    //    if (response == null || (response != null && response.Count == 0))
-    //    {
-    //        dynamic result = await _databaseManager.CreateAsync(entity, dbConnection, dbTransaction, dbType);
-    //        return result;
-    //    }
-    //    else
-    //    {
-    //        dynamic result = await _databaseManager.UpdateAsync(entity, nullable, whereClause, dbConnection, dbTransaction);
-    //        return result;
-    //    }
-    //}
 }
