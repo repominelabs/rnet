e1a24199e3d940391342dd4d8aa5242793f333a1
Ömer Çelik <oclk@outlook.com.tr>
2022-11-09 20:46:35 +0300

    refactor: Change dynamics to objects in interfaces

--- Source/Application/Interfaces/Repositories/IBaseRepository.cs
+++ Source/Application/Interfaces/Repositories/IBaseRepository.cs
@@ -2,14 +2,14 @@
 
 public interface IBaseRepository<T>
 {
-    dynamic Create(T entity);
-    Task<dynamic> CreateAsync(T entity);
-    dynamic CreateOrUpdate(T entity, bool nullable = false, string whereClause = null);
-    Task<dynamic> CreateOrUpdateAsync(T entity, bool nullable = false, string whereClause = null);
-    dynamic Delete(dynamic id = null, string whereClause = null);
-    Task<dynamic> DeleteAsync(dynamic id = null, string whereClause = null);
-    List<T> Read(dynamic id = null, string whereClause = null);
-    Task<List<T>> ReadAsync(dynamic id = null, string whereClause = null);
-    dynamic Update(T entity, bool nullable = false, string? whereClause = null);
-    Task<dynamic> UpdateAsync(T entity, bool nullable = false, string? whereClause = null);
+    object Create(T entity);
+    Task<object> CreateAsync(T entity);
+    object CreateOrUpdate(T entity, bool nullable = false, string whereClause = null);
+    Task<object> CreateOrUpdateAsync(T entity, bool nullable = false, string whereClause = null);
+    object Delete(object id = null, string whereClause = null);
+    Task<object> DeleteAsync(object id = null, string whereClause = null);
+    List<T> Read(object id = null, string whereClause = null);
+    Task<List<T>> ReadAsync(object id = null, string whereClause = null);
+    object Update(T entity, bool nullable = false, string? whereClause = null);
+    Task<object> UpdateAsync(T entity, bool nullable = false, string? whereClause = null);
 }
