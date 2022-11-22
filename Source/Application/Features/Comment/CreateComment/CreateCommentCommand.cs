6bb29c6b314b7281b679dc1482d3becf336748bc
Ömer Çelik <oclk@outlook.com.tr>
2022-11-23 00:21:24 +0300

    feat: Added rules for comment model

--- Source/Application/Features/Comment/CreateComment/CreateCommentCommand.cs
+++ Source/Application/Features/Comment/CreateComment/CreateCommentCommand.cs
@@ -1,5 +1,19 @@
-﻿namespace Application.Features.Comment.CreateComment;
+﻿using MediatR;
 
-public class CreateCommentCommand
+namespace Application.Features.Comment.CreateComment;
+
+public class CreateCommentCommand : IRequest<int>
 {
+    public int PostId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Body { get; set; }
+}
+
+public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommand, int>
+{
+    public Task<int> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Source/Application/Features/Comment/CreateComment/CreateCommentCommandValidator.cs
+++ Source/Application/Features/Comment/CreateComment/CreateCommentCommandValidator.cs
@@ -1,5 +1,16 @@
-﻿namespace Application.Features.Comment.CreateComment;
+﻿using FluentValidation;
 
-public class CreateCommentCommandValidator
+namespace Application.Features.Comment.CreateComment;
+
+public class CreateCommentCommandValidator : AbstractValidator<CreateCommentCommand>
 {
+	public CreateCommentCommandValidator()
+	{
+        RuleFor(v => v.PostId)
+            .NotEmpty();
+
+        RuleFor(v => v.Name)
+            .MaximumLength(200)
+            .NotEmpty();
+    }
 }
