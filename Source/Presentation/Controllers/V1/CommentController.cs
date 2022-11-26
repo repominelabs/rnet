102c52d502fa89facced00e480df1e76201f5f28
Ömer Çelik <oclk@outlook.com.tr>
2022-11-26 11:52:13 +0300

    feat: Created CommentController V1

---
+++ Source/Presentation/Controllers/V1/CommentController.cs
@@ -0,0 +1,15 @@
+﻿using Application.Features.Comment.CreateComment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers.V1;
+
+[Route("Api/V{version:apiVersion}/[controller]/[action]")]
+[ApiVersion("1.0", Deprecated = true)]
+public class CommentController : BaseController
+{
+    [HttpPost]
+    public async Task<ActionResult<int>> Create(CreateCommentCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+}
