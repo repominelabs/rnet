58c04d9b790b6c7229d76677c36739e565ce4d0b
Ömer Çelik <oclk@outlook.com.tr>
2023-01-19 01:16:18 +0300

    feat: Create create command of config

---
+++ Source/Application/Features/Configuration/CreateConfiguration/CreateConfigurationCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Application.Features.Configuration.CreateConfiguration;
+
+public class CreateConfigurationCommand : IRequest<object>
+{
+}
+
+public class CreateConfigurationCommandHandler : IRequestHandler<CreateConfigurationCommand, object>
+{
+    public Task<object> Handle(CreateConfigurationCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Source/Application/Features/Configuration/CreateConfiguration/CreateConfigurationCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Configuration.CreateConfiguration;
+
+public class CreateConfigurationCommandValidator : AbstractValidator<CreateConfigurationCommand>
+{
+	public CreateConfigurationCommandValidator()
+	{
+        //RuleFor(v => v.PostId)
+        //    .NotEmpty();
+
+        //RuleFor(v => v.Name)
+        //    .MaximumLength(200)
+        //    .NotEmpty();
+    }
+}
