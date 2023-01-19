f39c34e97cd30a21ceac74b2d65637b08d1dad32
Ömer Çelik <oclk@outlook.com.tr>
2023-01-19 10:41:39 +0300

    feat: Delete ops added

--- Source/Application/Features/Configuration/DeleteConfiguration/DeleteConfigurationCommand.cs
+++ Source/Application/Features/Configuration/DeleteConfiguration/DeleteConfigurationCommand.cs
@@ -1,16 +1,36 @@
-﻿using MediatR;
+﻿using Application.Interfaces.Repositories;
+using AutoMapper;
+using MediatR;
 
 namespace Application.Features.Configuration.DeleteConfiguration;
 
 public class DeleteConfigurationCommand : IRequest<object>
 {
     public long Id { get; set; }
+    public string WhereClause { get; set; }
 }
 
 public class DeleteConfigurationCommandHandler : IRequestHandler<DeleteConfigurationCommand, object>
 {
-    public Task<object> Handle(DeleteConfigurationCommand request, CancellationToken cancellationToken)
+    private readonly IConfigurationRepository _configurationRepository;
+    private readonly IMapper _mapper;
+
+    public DeleteConfigurationCommandHandler(IConfigurationRepository configurationRepository, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _configurationRepository = configurationRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<object> Handle(DeleteConfigurationCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var response = await _configurationRepository.DeleteAsync(request.Id, request.WhereClause);
+            return response;
+        }
+        catch (Exception)
+        {
+            return null;
+        }
     }
 }
