8c6964df245e8f2155e1806517ef3e8b89f86a4e
Ömer Çelik <oclk@outlook.com.tr>
2023-01-19 10:25:37 +0300

    reafactor: Improve GetValue method the new one

--- Source/Infrastructure/Persistence/Common/ConfigManager.cs
+++ Source/Infrastructure/Persistence/Common/ConfigManager.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Cinis.PostgreSql;
+using Domain.Entities;
 using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.Persistence.Common;
@@ -18,11 +19,10 @@ public sealed class ConfigManager
     private static readonly Lazy<List<Configuration>> _instanceConfigurationDb =
         new(() =>
         {
-            List<Configuration> configurations = new();
-
-            // Todo
-
-            return configurations;
+            using var connection = ConnectionFactory.CreateDbConnection("");
+            connection.Open();
+            var response = connection.Read<Configuration>(whereClause: "is_active = '1'");
+            return response;
         });
 
     private ConfigManager() { }
@@ -37,5 +37,37 @@ public sealed class ConfigManager
         T value = Configuration.GetValue<T>(key);
 
         return value;
+    }
+
+    public static List<Configuration> GetValue(string configType = null, string configValue1 = null, string configValue2 = null, string key = null)
+    {
+        if (ConfigurationDb != null)
+        {
+            List<Configuration> tempList = new(ConfigurationDb);
+
+            if (!string.IsNullOrEmpty(configType))
+            {
+                tempList = tempList.Where(x => x.ConfigType == configType)?.ToList();
+            }
+
+            if (!string.IsNullOrEmpty(configValue1))
+            {
+                tempList = tempList.Where(x => x.ConfigValue1 == configValue1)?.ToList();
+            }
+
+            if (!string.IsNullOrEmpty(configValue2))
+            {
+                tempList = tempList.Where(x => x.ConfigValue2 == configValue2)?.ToList();
+            }
+
+            if (!string.IsNullOrEmpty(key))
+            {
+                tempList = tempList.Where(x => x.Key == key)?.ToList();
+            }
+
+            return tempList;
+        }
+
+        return null;
     }
 }
