ea32e7d9300a29c5fb2f774a6b6d6a6c7458396a
Ömer Çelik <oclk@outlook.com.tr>
2023-01-19 10:03:51 +0300

    feat: Created Config model

--- Source/Domain/Entities/Configuration.cs
+++ Source/Domain/Entities/Configuration.cs
@@ -6,4 +6,27 @@ namespace Domain.Entities;
 [Table("configuration")]
 public class Configuration : BaseAuditableEntity
 {
+    [Column("application")]
+    public string Application { get; set; }
+
+    [Column("config_type")]
+    public string ConfigType { get; set; }
+
+    [Column("config_value_1")]
+    public string ConfigValue1 { get; set; }
+
+    [Column("config_value_2")]
+    public string ConfigValue2 { get; set; }
+
+    [Column("key")]
+    public string Key { get; set; }
+
+    [Column("value")]
+    public string Value { get; set; }
+
+    [Column("is_active")]
+    public string IsActive { get; set; }
+
+    [Column("description")]
+    public string Description { get; set; }
 }
